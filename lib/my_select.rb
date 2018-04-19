3fda8183de2a24f6d1449232ccd70551e70c8322
Chris Alberti <christopherjalberti@gmail.com>
2018-04-19 19:44:39 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -5,10 +5,6 @@ def my_select(collection)
   while i < collection.length
     ele = collection[i]
     result << ele if yield ele
-
-
-    new_ele = yield collection[i]
-    result << collection[i] if new_ele
     i += 1
   end
   result
